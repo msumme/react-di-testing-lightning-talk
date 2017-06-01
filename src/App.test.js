b08e5701ab7e3036a69003e3b438311115d7ab0c
Max Summe <msumme@cj.com>
2017-05-31 17:30:48 -0700

    Writing incomplete test to show problem with dependency injection

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('loads some content when I click a button', () => {
+
+  // given
+  const wrapper = shallow(<App />);
+  const contentToLoad = "Some new content";
+  expect(wrapper.find('.content').text()).toEqual("Initial Content");
+
+    // when
+  wrapper.find(Button).simulate('click');
+  wrapper.update();
+
+  // then
+
+  expect(wrapper.find('.content').text()).toEqual(contentToLoad);
+
 });
