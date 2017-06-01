bf29a87128d05c6dba3af1239aa126aa39ec1c47
Max Summe <msumme@cj.com>
2017-05-31 17:33:01 -0700

    Writing part of implementation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: "Initial Content"
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +19,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="content App-intro">
+          {this.state.content}
         </p>
+        <button>Click me!</button>
       </div>
     );
   }
